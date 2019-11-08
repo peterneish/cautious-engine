aa655a5be3ff70e8c5a027cbeba81353a8e95973
Peter Neish <peter.neish@gmail.com>
2019-11-08 16:37:47 +1100

    adding teh Description with holepunch

---
+++ README.md
@@ -0,0 +1,7 @@
+
+# cautious-engine
+
+This is a test projec that we created at AIMOS2019
+
+
+
--- analysis.R
+++ analysis.R
@@ -1,2 +1,20 @@
 # Analysis script for my project
 
+#create some data
+apples <- data.frame(apple_id = as.factor(1:1000), 
+                    weight = rnorm(1000,5,1))
+write.csv(apples, 'data/apples_data.csv', row.names = FALSE)
+
+#read data
+apples <- read.csv('data/apples_data.csv')
+
+#process data
+apples$log_weight <-(log(apples$weight))
+
+# graphing
+png('figs/hist_apple_weight.png') # makes the plot device the png file instead of the plot window
+ggplot2::ggplot(apples, aes(weight)) + geom_histogram()
+dev.off() # turns off the plot device
+
+# we ran some of these commands in the tutorial
+#usethis::use_readme_md()
